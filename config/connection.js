d711eb285ee4a22fdc91c4190202cba373633342
GarretR <garret.rueckert.guitar@gmail.com>
2019-02-20 18:17:50 -0700

    Finished, need readme

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+//MySQL Connection file
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "7890",
+  database: "burgers_db"
+});
+
+module.exports = connection;
